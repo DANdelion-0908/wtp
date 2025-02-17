7f4f8ff072c4e9d60467038ac1e32c87e8ffe25a
Daniel Penedo <pen22217@uvg.edu.gt>
2025-02-16 19:48:19 -0600

    Add required dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,5 +14,10 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["coffee"],
+  },
 } satisfies Config;
+
+export default config;
