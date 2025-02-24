520994b35f09d246be2670f4a6224a18a43dea93
Eunice Mata <eunice.mata2992@gmail.com>
2025-02-23 19:06:06 -0600

    createUser function

--- src/functions/node_creation_functions.js
+++ src/functions/node_creation_functions.js
@@ -34,3 +34,45 @@ async function createPost(driver, text, imagen, hashtags, reposted = false) {
         await session.close();
     }
 }
+
+async function createUser(driver, username, password, email, born, first_name, last_name, gender) {
+    /**
+     * Creates a User node with specified attributes.
+     *
+     * @param {neo4j.Driver} driver - The Neo4j driver instance.
+     * @param {string} username - The username of the user.
+     * @param {string} password - The password of the user.
+     * @param {string} email - The email of the user.
+     * @param {Date} born - The birthdate of the user. Format: YYYY-MM-DD
+     * @param {string} first_name - The first name of the user.
+     * @param {string} last_name - The last name of the user.
+     * @param {string} gender - The gender of the user (Female, Male, NonBinary, NotSpecified).
+     */
+
+    const label = "User";
+    const query = `
+        CREATE (u:${label} {
+            username: $username,
+            password: $password,
+            email: $email,
+            born: $born,
+            first_name: $first_name,
+            last_name: $last_name,
+            gender: $gender,
+            followers: 0,
+            following: 0,
+            verified: false
+        })
+    `;
+
+    const session = driver.session({ database: 'neo4j' });
+
+    try {
+        await session.run(query, { username, password, email, born, first_name, last_name, gender });
+        console.log("User created successfully.");
+    } catch (error) {
+        console.error("Error executing query:", error);
+    } finally {
+        await session.close();
+    }
+}
