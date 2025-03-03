a4f56c67966582bccef07c36574d2f99d3e47204
chuy-zip <andreschuy03@gmail.com>
2025-03-02 21:47:17 -0600

    right side now visible

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,12 +3,12 @@
 import Feed from '@/components/feed'
 import { LeftSideMenu } from '@/components/leftSideMenu'
 import { RightSideMenu } from '@/components/rightSideMenu'
-import { Header} from '@/components/header'
+import { Header } from '@/components/header'
 import React, { useState } from 'react'
 import { UserPage } from '@/components/userPage'
 import { useRouter } from 'next/navigation'
 
-export default function Dashboard({handleAuth}: any) {
+export default function Dashboard({ handleAuth }: any) {
   const [isProfileActive, setProfileActive] = useState(true);
   const router = useRouter();
 
@@ -16,39 +16,40 @@ export default function Dashboard({handleAuth}: any) {
     switch (isProfileActive) {
       case false:
         setProfileActive(true);
-      break;
-  
+        break;
+
       case true:
         setProfileActive(false);
-      break;
+        break;
     }
   };
 
   const handleFeed = () => {
     setProfileActive(false);
-    
+
   }
   return (
     <>
-      <Header handleProfile={handleProfile} setProfileActive={handleFeed} handleAuth={handleAuth}/>
+      <Header handleProfile={handleProfile} setProfileActive={handleFeed} handleAuth={handleAuth} />
       <div className='flex flex-row mt-auto overflow-y-hidden fixed top-[10%] h-[100vh] w-full'>
         <div className='w-[30%] h-auto'>
-          <LeftSideMenu setProfileActive={handleFeed}/>
+          <LeftSideMenu setProfileActive={handleFeed} />
         </div>
         {isProfileActive === false ? (
           <>
             <div className='w-[40%] h-auto pb-[5%] overflow-x-hidden overflow-y-auto>'>
-            <Feed />
+              <Feed />
             </div>
-            <div className='w-[30%] h-full flex flex-col items-center text-center'>
-              <RightSideMenu />
-            </div>
+
           </>
         ) : (
-          <UserPage 
+          <UserPage
             currentProfilePicture='/eula.jpg'
-            currentUserName='dandelion'/>
+            currentUserName='dandelion' />
         )}
+        <div className='w-[30%] h-full flex flex-col items-center text-center'>
+          <RightSideMenu />
+        </div>
       </div>
     </>
   )
--- src/components/rightSideMenu.tsx
+++ src/components/rightSideMenu.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const RightSideMenu = () => {
   return (
-    <div className='bg-base-300 rounded-box w-[50%] h-full mr-auto ml-[2%]'>a</div>
+    <div className='bg-base-300 rounded-box w-[50%] h-full mr-auto ml-[2%] border-2 border-red-500'>
+      buenas noches
+      <button>hi</button>
+    </div>
   )
 }
